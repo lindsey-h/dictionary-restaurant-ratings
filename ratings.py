544d3aecc2b1eed327c50a4e598f1040a0f0d89a
Lindsey Hight <lindseydhight@gmail.com>
2021-05-03 20:55:57 +0000

    Finished ratings.py assignment
    Co-authored-by: Arielle Nugent <ariellenugent@gmail.com>

--- ratings.py
+++ ratings.py
@@ -1,4 +1,42 @@
 """Restaurant rating lister."""
 
+def read_ratings(file_path):
 
-# put your code here
+    restaurant_details = open(file_path)
+
+    restaurants = {}
+
+    for line in restaurant_details:
+
+        line = line.rstrip()
+        details = line.split(":")
+        restaurant_name, restaurant_rating = details
+
+        restaurants[restaurant_name] = restaurant_rating
+
+    #ask user for input = name
+    name = input("What restaurant do you want to add? ")
+    name = name.title()
+    #ask user for input = score
+    rating = input("What is the rating? ")
+    #take input and add into dict
+    restaurants[name] = rating
+    #print in alphabetical order
+    
+    #list = sorted(dict)
+    alpha_rests = sorted(restaurants)
+    #loop through list
+    for restaurant in alpha_rests:
+        print(f"{restaurant} is rated at {restaurants[restaurant]}.")
+    # print rname and dict[rname]=rating
+
+    restaurant_details.close()
+
+    
+
+
+read_ratings("scores.txt")
+
+
+
+
